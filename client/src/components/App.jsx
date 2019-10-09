267a5bc12ec9efcc9e103b2fe8ff8ca4b0e2f571
Jose Valadez <vinmajj@gmail.com>
2019-10-08 18:52:31 -0700

    build boilerplate

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        )
+    }
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname,'/client/src');
+const DIST_DIR = path.join(__dirname,'client/dist');
+// entry: `${SRC_DIR}/App.jsx` // changed to have index.js be entry
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  performance: { hints: false },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+           options: {
+            "presets": [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+           }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader","css-loader"]
+      }
+    ]
+  }
+};
